fe31ab4ed2e31f66767208d4fbc13fd921de1d5e
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-04-02 05:55:12 +0200

    Update metrics.py

--- fastai_object_detection/metrics.py
+++ fastai_object_detection/metrics.py
@@ -2,6 +2,8 @@
 from mean_average_precision import MetricBuilder
 #from fastai.metrics import AvgMetric
 from fastai.metrics import Metric
+from fastai.torch_basics import *
+from fastai.torch_core import *
 from functools import partial
 
 __all__ = ['mAP_at_IoU40','mAP_at_IoU90']# 'mAP_at_IoU50', 'mAP_at_IoU60', 'mAP_at_IoU70', 'mAP_at_IoU80', ]
