73042b8959046e4b14822a353cd09dff933504f7
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-04-02 07:22:02 +0200

    Update learners.py

--- fastai_object_detection/learners.py
+++ fastai_object_detection/learners.py
@@ -8,30 +8,21 @@ __all__ = ['fastrcnn_learner']
 
 class fastrcnn_learner(Learner):
     def __init__(self, dls, model, cbs=None, **kwargs):
-        if cbs is not None:
-            cbs = L(FasterRCNNAdapter())+L(cbs)
-        else: 
-            cbs = [FasterRCNNAdapter()]
-            
+        if cbs is not None: cbs = L(FasterRCNNAdapter())+L(cbs)
+        else: cbs = [FasterRCNNAdapter()]
         model = model(num_classes=len(dls.vocab))
-            
         super().__init__(dls, model, loss_func=noop, cbs=cbs, **kwargs)
         
     def get_preds(self, items, dl=None, item_tfms=None, batch_tfms=None):
-        
         if item_tfms is None: item_tfms = [Resize(800)]
-        
         if dl is None:
             dblock = DataBlock(
                 blocks=(ImageBlock(cls=PILImage)),
                 item_tfms=item_tfms,
                 batch_tfms=batch_tfms)
-
             test_dl = dblock.dataloaders(items).test_dl(items, bs=self.dls.bs)
         else: test_dl = dl
-
-        inputs = []
-        preds = []
+        inputs,preds = [],[]
         with torch.no_grad():
             for i,batch in enumerate(progress_bar(test_dl)):
                 self.model.eval()
@@ -60,7 +51,7 @@ class fastrcnn_learner(Learner):
         for idx in range(len(inputs)):
             if idx >= max_n: break
             fig, ax = plt.subplots(figsize=(8,8))
-            TensorImage(inp[idx]).show(ax=ax)
+            TensorImage(inputs[idx]).show(ax=ax)
             LabeledBBox(TensorBBox(bboxes[idx]), [self.dls.vocab[int(l.item())] 
                                                   for l in labels[idx]]).show(ax)
         
