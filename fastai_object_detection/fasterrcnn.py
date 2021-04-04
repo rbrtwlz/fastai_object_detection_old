b212465bba47568c8d4da3a43e04d930d191463e
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-04-04 02:50:38 +0200

    Update fasterrcnn.py

--- fastai_object_detection/fasterrcnn.py
+++ fastai_object_detection/fasterrcnn.py
@@ -1,13 +1,16 @@
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
+from torchvision.models.utils import load_state_dict_from_url
 from torchvision.models.detection.anchor_utils import AnchorGenerator
 from torchvision.models.detection import FasterRCNN
 from functools import partial
 
 __all__ = ['get_FasterRCNN', 'fasterrcnn_resnet18', 'fasterrcnn_resnet34', 'fasterrcnn_resnet50', 'fasterrcnn_resnet101', 'fasterrcnn_resnet152']
 
-def get_FasterRCNN(arch_str, num_classes, pretrained=True, **kwargs):
+def get_FasterRCNN(arch_str, num_classes, pretrained=True, pretrained_backbone=True, **kwargs):
     
-    backbone = resnet_fpn_backbone(arch_str, pretrained=pretrained, trainable_layers=5)
+    #if pretrained == True: pretrained_backbone=False
+        
+    backbone = resnet_fpn_backbone(arch_str, pretrained=pretrained_backbone, trainable_layers=5)
     
     anchor_sizes = ((16,), (32,), (64,), (128,), (256,),)
     aspect_ratios = ((0.5, 1.0, 2.0),) * len(anchor_sizes)
@@ -22,6 +25,14 @@ def get_FasterRCNN(arch_str, num_classes, pretrained=True, **kwargs):
                        #box_score_thresh=0.6,
                        **kwargs
                       )
+    
+    if pretrained:
+        try:
+            state_dict = load_state_dict_from_url(model_urls[arch_str+'_fpn_coco'],
+                                              progress=progress)
+            model.load_state_dict(state_dict)
+        except: 
+            print("No pretrained model found for "+arch_str)
     
     return model.train()
 
