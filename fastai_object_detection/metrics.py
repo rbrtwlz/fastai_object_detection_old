27b942dab401c81729fa2781eca93afb0602f6a1
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-04-06 22:13:21 +0200

    Update metrics.py

--- fastai_object_detection/metrics.py
+++ fastai_object_detection/metrics.py
@@ -9,32 +9,6 @@ from functools import partial
 
 __all__ = ['mAP_at_IoU40', 'mAP_at_IoU50', 'mAP_at_IoU60', 'mAP_at_IoU70', 'mAP_at_IoU80', 'mAP_at_IoU90']
 
-"""
-def create_metric_samples(preds, targs):
-    pred_samples = []
-    for pred in preds:
-        res = torch.cat([pred["boxes"], pred["labels"].unsqueeze(-1), pred["scores"].unsqueeze(-1)], dim=1) 
-        pred_np = np.array(res.detach().cpu())
-        pred_samples.append(pred_np)
-
-    targ_samples = []
-    for targ in targs: # targs : yb[0]
-        #print(targ["boxes"].shape,targ["labels"].shape)
-        targ = torch.cat([targ["boxes"],targ["labels"].unsqueeze(-1)], dim=1)
-        targ = torch.cat([targ, torch.zeros([targ.shape[0], 2], device=targ.device)], dim=1)
-        targ_np = np.array(targ.detach().cpu())
-        targ_samples.append(targ_np)
-
-    return [i for i in zip(pred_samples, targ_samples)]
-
-
-def m_ap_metric(preds, targs, num_classes, iou_thresholds=0.4):
-    metric_fn = MetricBuilder.build_evaluation_metric("map_2d", async_mode=True, num_classes=num_classes)
-    for sample_preds, sample_targs in create_metric_samples(preds, targs):
-        metric_fn.add(sample_preds, sample_targs)
-    metric_batch =  metric_fn.value(iou_thresholds=iou_thresholds)['mAP']
-    return metric_batch
-"""
 
 class mAP_Metric():
     def __init__(self, iou_thresholds, name):
@@ -72,7 +46,6 @@ class AvgMetric_Copy(Metric):
     def reset(self): self.total,self.count = 0.,0
     def accumulate(self, learn):
         bs = len(learn.xb[0])
-        print("bs:",bs)
         self.total += learn.to_detach(self.func(learn.pred, *learn.yb, num_classes=len(learn.dls.vocab)))*bs
         self.count += bs
     @property
