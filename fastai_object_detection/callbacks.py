140a400d498d1bf2939a559bb4945413159b13de
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-04-07 03:44:31 +0200

    Update callbacks.py

--- fastai_object_detection/callbacks.py
+++ fastai_object_detection/callbacks.py
@@ -1,7 +1,6 @@
 from fastai.callback.all import *
 from fastai.torch_basics import *
 from fastai.torch_core import *
-from datetime import datetime
 
 __all__ = ['RCNNAdapter']
 
@@ -15,11 +14,6 @@ class RCNNAdapter(Callback):
         self.learn.save_yb = []
 
     def before_batch(self):
-     
-        now = datetime.now()
-        current_time = now.strftime("%H:%M:%S")
-        print("Current Time =", current_time)
-        
         self.learn.save_xb = self.learn.xb
         self.learn.save_yb = self.learn.yb
         
