9102a5f696db8be737a22f1032e1b38e3624bb14
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-06-05 17:23:23 +0200

    Update learners.py

--- fastai_object_detection/learners.py
+++ fastai_object_detection/learners.py
@@ -8,7 +8,7 @@ __all__ = ['fasterrcnn_learner', 'maskrcnn_learner']
 
 def rcnn_split(m):
     "Default split of rcnn model between transform,backbone and rpn,roi_heads"
-    m = L(c for c in m.children())
+    m = L(c.children() for c in m.children())
     return L(m[:2], m[2:]).map(params)
     #return L(m).map(params)
 
