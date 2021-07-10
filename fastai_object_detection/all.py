c1045a760070e73189be981d7909b4e89b4cc89b
rbrtwlz <81785368+rbrtwlz@users.noreply.github.com>
2021-07-11 00:45:23 +0200

    Update all.py

--- fastai_object_detection/all.py
+++ fastai_object_detection/all.py
@@ -3,6 +3,6 @@ from .callbacks import *
 from .learners import *
 from .dataloaders import *
 from .models.all import *
-from .metrics import *
+from .metrics.all import *
 from .helpers import *
 from .datasets import *
